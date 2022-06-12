7c768c178c9149065a91e1ec238cc6fa68f35916
Santiago Garreta <santiagogarreta6@gmail.com>
2022-06-12 18:14:13 -0300

    update implementacion bootstrap

---
+++ Scripts/Script.js
@@ -0,0 +1,4 @@
+fetch('https://api.rawg.io/docs/?format=openapi').then(response => response.json()).then(data => console.log(data));
+
+// document.getElementById('IDmain__mainText').innerHTML = "<p>new content</p>";
+
